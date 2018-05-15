63b79e2219886ddd37457e20ed3d40d560fc1d7a
Revilo1882 <oliverrharris@gmail.com>
2018-05-15 16:41:33 +0100

    adds stormy weather

--- src/Airport.js
+++ src/Airport.js
@@ -8,5 +8,13 @@ Airport.prototype.hangar = function() { return this._hangar;
 Airport.prototype.clearForLanding = function(plane) { this._hangar.push(plane);
 };
 
-Airport.prototype.clearForTakeOff = function(plane) { this._hangar = [];
+Airport.prototype.clearForTakeOff = function(plane) {
+  if(this.isStormy()) {
+    throw new Error('cannot takeoff during storm');
+  }
+  this._hangar = [];
+};
+
+Airport.prototype.isStormy = function() {
+  return false;
 };
